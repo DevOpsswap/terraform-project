df396c8e551385efbcc221870b1c9a792f8d7089
Swapnil <devops.swap@gmail.com>
2026-04-03 17:20:12 +0530

    new

---
+++ resource.tf
@@ -0,0 +1,24 @@
+resource "aws_instance" "webserver" {
+  ami                     = "var.instanceami"
+  instance_type           = "var.instancetype"
+  vpc_security_group_ids  = ["var.sg", "aws_security_group.webserversg.id" , "data.aws_security_group.sg_gui.id"]
+  key_name                = "var.keyname"
+  count                   = var.nosofinstance
+  disable_api_termination = var.api_termination
+
+  tags = {
+    Name = "webserver_instance"
+    purpose = "learning-terraform"
+  }
+
+user_data = <<-EOF
+            #!/bin/bash
+            sudo -i
+            apt install apache2 -y
+            systemctl start apache2
+            EOF
+}
+
+
+data "aws_security_group" "sg_gui"
+  name   = "rdssecurity"
---
+++ variable.tf
@@ -0,0 +1,29 @@
+variable "instanceami" {
+    default =   "ami-05d2d839d4f73aafb"
+} 
+
+variable "instancetype" {
+   default = "t3.micro"
+}
+
+variable "keyname" {
+   default = "ubuntu"
+}
+
+variable "sg" {
+    default = "sg-05520a2ca525d2bc3"
+} 
+variable "api_termination" {
+  default = true
+} 
+
+variable "nosofinstances" {
+  default = 2
+}
+
+
+#data types 
+#  1    =   number 
+#  "anup", "1" = string 
+#  true  = boolean 
+#  2.1 = float  
